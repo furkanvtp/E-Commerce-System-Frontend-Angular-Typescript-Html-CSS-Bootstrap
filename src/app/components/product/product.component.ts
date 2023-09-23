8874058cabc2de28cc7e710b70cdefcf347db020
Furkan Küçük <furkankucuk146@gmail.com>
2023-09-23 20:29:41 +0300

    Api baglantisi yapiliyor

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
+import { HttpClient } from '@angular/common/http';
+import { ProductResponseModel } from 'src/app/models/productResponseModel';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
+  products: Product[] = []; //bu da array yani liste
+  apiUrl = 'https://localhost:44302/api/products/getall';
+  constructor(private httpClient: HttpClient) {} //productcomponentin instancesını üretmeye yarar yani newler
 
-  products:Product[] = []; //bu da array yani liste
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.httpClient
+      .get<ProductResponseModel>(this.apiUrl)
+      .subscribe((response) => {
+        this.products = response.data
+      });
+  }
 }
